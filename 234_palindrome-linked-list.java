ebaa6d0845abb984ae983ecbe4327486b8e2a295
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2021-08-29 22:30:58 -0400

    add 234

---
+++ 234_palindrome-linked-list.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+        
+        ListNode fast = head, slow = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast != null)
+            slow = slow.next;
+        
+        ListNode p1 = head;
+        ListNode p2 = reverseLinkedList(slow);
+        
+        while(p2 != null){
+            if(p1.val != p2.val) return false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return true;
+    }
+    
+    public ListNode reverseLinkedList(ListNode head){
+        if (head == null || head.next == null) return head;
+        
+        ListNode prev = null, curr = head, next = head;
+        
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
