0e590ce9b2fe3c3942c3e8a9b7d79f14a4f5c800
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2021-09-24 11:03:41 -0400

    add 217

---
+++ 217_contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0 || nums.length == 1) return false;
+        
+        Set<Integer> set = new HashSet<>();
+        for(int n: nums){
+            if(set.contains(n)) return true;
+            set.add(n);
+        }
+        return false;
+    }
+}
