9b56b22bc92a532ae6d08e00a3c6ded5f2ec7615
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2021-09-28 11:23:05 -0400

    add 202

---
+++ 202_happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (!set.contains(n)) {
+            set.add(n);
+            n = squareSum(n);
+            if (n == 1) {
+                return true;
+            }
+        }
+        return n == 1;
+    }
+    
+    private int squareSum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
