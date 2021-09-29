2d2bebfc57049be7aa15bf53cb9534464111c63d
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2021-09-28 20:01:12 -0400

    add 290

---
+++ 290_word-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] strs = s.split(" ");
+        HashMap<Character, String> hm = new HashMap<>();
+        
+        if (pattern.length() != strs.length)    
+            return false;
+
+        for (int i = 0; i < pattern.length(); i ++){
+            char c = pattern.charAt(i);
+            if (hm.containsKey(c)){
+                if (!hm.get(c).equals(strs[i]))
+                    return false;
+            }else{
+                if (hm.containsValue(strs[i]))
+                    return false;
+                hm.put(c, strs[i]);
+            }
+        }
+        return true;
+    }
+}
