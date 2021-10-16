4fe12aefc552d570dbc74a123da66d8afc2f2a59
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2021-10-15 22:00:54 -0400

    update

--- 48_rotate-image.java
+++ 48_rotate-image.java
@@ -27,34 +27,4 @@ class Solution {
             }
         }
     }
-    
-    
 }
-/* Clockwise rotation
-
-   Original            Transposed                Reversed
-1  2  3  4  5         1  6  11 16 21         21 16 11  6  1  
-6  7  8  9  10        2  7  12 17 22         22 17 12  7  2
-11 12 13 14 15    =>  3  8  13 18 23   =>    23 18 13  8  3
-16 17 18 19 20        4  9  14 19 24         24 19 14  9  4
-21 22 23 24 25        5  10 15 20 25         25 20 15  10 5
-
-*/
-
-/*
-    a common method to solve the image rotation problems.
-    /*
-     * CLOCKWISE rotate
-     * first reverse up to down, then swap the symmetry 
-     * 1 2 3     7 8 9     7 4 1
-     * 4 5 6  => 4 5 6  => 8 5 2
-     * 7 8 9     1 2 3     9 6 3
-    */
-
-    /*
-     * ANTICLOCKWISE rotate
-     * first reverse left to right, then swap the symmetry
-     * 1 2 3     3 2 1     3 6 9
-     * 4 5 6  => 6 5 4  => 2 5 8
-     * 7 8 9     9 8 7     1 4 7
-    */
