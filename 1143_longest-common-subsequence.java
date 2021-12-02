64f3cefe80716eebe4d2e90e87bf76b377e4ffe1
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2021-12-01 22:42:03 -0500

    add 1143

---
+++ 1143_longest-common-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (int row = 1; row <= m; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (text1.charAt(row - 1) == text2.charAt(col - 1)) {
+                    // if previous equal, next is (diagonal + 1)
+                    dp[row][col] = dp[row - 1][col - 1] + 1;
+                } else {
+                    // if previous not equal, get the max of previous row and previous col
+                    dp[row][col] = Math.max(dp[row - 1][col], dp[row][col - 1]);
+                }               
+            }
+        }
+        return dp[m][n];
+    }
+}
