9e2ed8837b11738a0ea03e867f5bac10bbb8c6d2
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2022-01-22 14:37:43 -0500

    update

--- 29_divide-two-integers.java
+++ 29_divide-two-integers.java
@@ -1,34 +1,50 @@
 class Solution {
-    public int divide(int dividend, int divisor) {
-        int sign = 1;
-        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
-            sign = -1;
-
-        long ldividend = Math.abs((long) dividend);
-        long ldivisor = Math.abs((long) divisor);
-        
-        if (ldividend == 0 || (ldividend < divisor))    return 0;
-        if (divisor == 0)   return Integer.MAX_VALUE;
-        
-        long lans = ldivide(ldividend, ldivisor);
-        int res = 0;
-        if (lans > Integer.MAX_VALUE){
-            res = sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }else{
-            res = (int)(sign * lans);
-        }
-        return res;
-    }
+    // Time: O(log2n); Space: O(1)
+    private static int HALF_INT_MIN = -1073741824;
     
-    private long ldivide (long ldividend, long ldivisor){
-        if (ldividend < ldivisor)   return 0;
-        long sum = ldivisor;
-        long multiple = 1;
-        while ((sum + sum) < ldividend){
-            sum += sum;
-            multiple += multiple;
+    public int divide(int dividend, int divisor) {
+        // Overflow
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+        
+        // Convert to negatives due to larger range avoiding overflow
+        int negatives = 2;
+        if (dividend > 0) {
+            negatives--;
+            dividend = -dividend;
         }
         
-        return multiple + ldivide(ldividend - sum, ldivisor);
+        if (divisor > 0) {
+            negatives--;
+            divisor = -divisor;
+        }
+        
+        int quotient = 0;
+        
+        /* Once divisor is larger than current dividend,
+         * can't fit any more copies of divisor into it. */
+        while (divisor >= dividend) {
+            
+            /* Fit at least once as divivend >= divisor.
+             * Use a negative powerOfTwo as for divide(INT_MIN, -1). */
+            int powerOfTwo = -1;
+            int value = divisor;
+            
+            // Check if double the current value is too big. If not, continue doubling.
+            while (value >= HALF_INT_MIN && value + value >= dividend) {
+                value += value;
+                powerOfTwo += powerOfTwo;
+            }
+            
+            // Subtract divisor another powerOfTwo times
+            quotient += powerOfTwo;
+            // Remove value so far, to continue process with remainder.
+            dividend -= value;
+        }
+        
+        if (negatives != 1)
+            return -quotient;
+        
+        return quotient;
     }
 }
