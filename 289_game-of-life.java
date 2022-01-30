f159cf81972d4589b21c2ef55ce1bf029212e388
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2022-01-30 17:59:26 -0500

    update

--- 289_game-of-life.java
+++ 289_game-of-life.java
@@ -2,8 +2,7 @@ class Solution {
 // Time: O(m*n); Space: O(1)
     public void gameOfLife(int[][] board) {
         if (board == null || board.length == 0 || board[0].length == 0)
-            return;
-        
+            return;   
         int m = board.length, n = board[0].length;
         
         // Positions of neighbor
@@ -27,25 +26,21 @@ class Solution {
                             int colOfNeighbor = col + shift[j];
                             
                             // Check the state of neighbor
-                            // Math.abs() to get the original absolute value of cell
-                            
+                            // Math.abs() to get the original absolute value of cell    
                             if (rowOfNeighbor >= 0 && rowOfNeighbor < m && colOfNeighbor >= 0 && colOfNeighbor < n && 
                                 Math.abs(board[rowOfNeighbor][colOfNeighbor]) == 1)
                                 liveCells++;
                         } 
                     }
                 }
-                
                 // Live cells--> now dead cells
                 if ((board[row][col] == 1) && (liveCells < 2 || liveCells > 3))
                     board[row][col] = -1;
-                
                 // Dead cells--> now live cells
                 if (board[row][col] == 0 && liveCells == 3)
                     board[row][col] = 2;
             }
         }
-        
         // Update the matrix
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
@@ -53,7 +48,6 @@ class Solution {
                 // All positive cells
                 if (board[row][col] > 0) 
                     board[row][col] = 1;
-                
                 // Others, including negative cells and 0 cells
                 else
                     board[row][col] = 0;
