72e2d6533a38268f209a8478e747327f55d057b0
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2022-02-08 17:02:24 -0500

    update

--- 23_merge-k-sorted-lists.java
+++ 23_merge-k-sorted-lists.java
@@ -9,6 +9,7 @@
  * }
  */
 class Solution {
+    /*
     // Time: O(nlogk); Space: O(n)
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0)
@@ -37,5 +38,45 @@ class Solution {
                 heap.offer(curr.next);
         }
         return dummy.next;
+    }
+    */
+    
+    // Merge with divide and conquer
+    // Time: O(nlogk), k lists, n nodes;  Space: O(1)
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0)
+            return null;
+        
+        int interval = 1;
+        while (interval < lists.length) {
+            for (int i = 0; i+interval < lists.length; i=i+interval*2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i+interval]);
+            }
+            interval *= 2;
+        }
+        return lists[0];
+    }
+    
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode curr = new ListNode(0);
+        ListNode dummy = curr;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+            if (l1 != null)
+                curr.next = l1;
+            if (l2 != null)
+                curr.next = l2;
+            
+            return dummy.next;
     }
 }
