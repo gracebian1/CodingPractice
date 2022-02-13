3552e2f74fbf9e229051e4bdf043cca9846064ac
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2022-02-13 12:13:49 -0500

    update

--- 692_top-k-frequent-words.java
+++ 692_top-k-frequent-words.java
@@ -1,27 +1,32 @@
 class Solution {
+    // Time: O(nlogk); Space: O(n)
     public List<String> topKFrequent(String[] words, int k) {
-  
+        List<String> result = new ArrayList<>();
+        if (words == null || words.length == 0)
+            return result;
+        
         Map<String, Integer> map = new HashMap<>();
+        
         for (String word: words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
         
-        // Attention: ASCII, a:97, z:122
-        // Larger number should go after smaller number for lexicographical order
-        // s.compareTo(t) to compare two strings
-        // Make max heap
+        // MIN heap, with REVERSED lexicographical order
+        PriorityQueue<Map.Entry<String, Integer>> heap = new PriorityQueue<>(
+            (a, b) -> a.getValue() == b.getValue() ? b.getKey().compareTo(a.getKey()) : (a.getValue() - b.getValue()));
         
-        PriorityQueue<Map.Entry<String, Integer>> heap = new PriorityQueue<>
-            ((a, b) -> a.getValue() == b.getValue() ? a.getKey().compareTo(b.getKey()) : b.getValue() - a.getValue());
-        
+        // MIN heap, contains k elements, thus logk time
         for (Map.Entry<String, Integer> entry: map.entrySet()) {
             heap.offer(entry);
+            if(heap.size() > k)
+                heap.poll();
         }
         
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            list.add(heap.poll().getKey());
+        while (!heap.isEmpty()) {
+            result.add(heap.poll().getKey());
         }
-        return list;
+        
+        Collections.reverse(result);
+        return result;
     }
 }
