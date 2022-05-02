0a99f7e1488a4813ea669f87bfe5bb78aa846006
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2022-05-02 19:01:32 -0400

    update 56

--- 56_merge-intervals.java
+++ 56_merge-intervals.java
@@ -1,25 +1,25 @@
 class Solution {
+    // Time: O(nlogn); Space: O(logn)
     public int[][] merge(int[][] intervals) {
-        // Sort in ascending order by the first number 
+        if (intervals == null || intervals.length == 0)
+            return new int[][]{};
+        
+        if (intervals.length == 1)
+            return intervals;
+        
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         
-        // Use linked list to easily attach or merge to the tail
-        LinkedList<int[]> temp = new LinkedList<>();
+        LinkedList<int[]> list = new LinkedList<>();
         
         for (int[] interval: intervals) {
-            
-            // If list is empty, OR the last node's ending num < upcoming element's starting num
-            // Indicating non-overlapping, simply add the new element
-            
-            if (temp.isEmpty() || temp.getLast()[1] < interval[0]) {
-                temp.add(interval);
-                
-            // Else, update the ending num of the linked list            
+            if (list.isEmpty() || list.getLast()[1] < interval[0]) {
+                list.add(interval);
             } else {
-                temp.getLast()[1] = Math.max(temp.getLast()[1], interval[1]);
+                list.getLast()[1] = Math.max(interval[1], list.getLast()[1]);
             }
         }
-        // Return the linked list in array
-        return temp.toArray(new int[temp.size()][]);
+        
+        int[][] result = new int[list.size()][];      
+        return list.toArray(result);
     }
 }
