0e636ccd014eb5073c8202af7f16b7e8b11a1b9f
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2022-05-09 22:06:03 -0400

    update 244

--- 244_shortest-word-distance-ii.java
+++ 244_shortest-word-distance-ii.java
@@ -1,47 +1,48 @@
 class WordDistance {
-    
-    // Map key: word; Map value: list of indices for the same word
-    private Map<String, List<Integer>> map;
+     /* Time: O(n): total number of words; Space:O(n)
+        Time: O(max(n1, n2)): n1, n2: # of occurrences of two words. n1 = n2 = O(n).
+        Why max: when the minimum element of the shorter list is larger than all elements of the longer list. 
+        The pointer for the short list will not progress at all, and the pointer for the longer list will 
+        reach to the very end.
+     */
+
+    // Map key: String word; Map value: list of indices
+    Map<String, List<Integer>> map;
 
     public WordDistance(String[] wordsDict) {
-        this.map = new HashMap<String, List<Integer>>();
+        this.map = new HashMap<>();
         
-        for (int index = 0; index < wordsDict.length; index++) {
-            String word = wordsDict[index]; 
-            
-            if (map.containsKey(word)) {
-                map.get(word).add(index);
-            } else {
-                List<Integer> list = new ArrayList<>();
-                list.add(index);
-                map.put(word, list);
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (!this.map.containsKey(wordsDict[i])) {
+                this.map.put(wordsDict[i], new ArrayList<>());
             }
-        }
+            List<Integer> temp = this.map.get(wordsDict[i]);
+            temp.add(i);
+            this.map.put(wordsDict[i], temp);
+        }  
     }
     
     public int shortest(String word1, String word2) {
-        List<Integer> list1 = map.get(word1);
-        List<Integer> list2 = map.get(word2);
+        List<Integer> list1 = this.map.get(word1);
+        List<Integer> list2 = this.map.get(word2);
         
-        int min = Integer.MAX_VALUE;
-        int i = 0, j = 0;
+        if (list1.size() == 1 && list2.size() == 1)
+            return Math.abs(list1.get(0) - list2.get(0));
         
-        while (i < list1.size() && j < list2.size()) {
-            int index1 = list1.get(i);
-            int index2 = list2.get(j);
+        int l1 = 0, l2 = 0, min = Integer.MAX_VALUE;
+        
+        while (l1 < list1.size() && l2 < list2.size()) {
+            min = Math.min(min, Math.abs(list1.get(l1) - list2.get(l2)));
             
-            min = Math.min(min, Math.abs(index1 - index2));
-            
-            // Both lists are sorted 
-            if (index1 < index2)
-                i++;
-            else 
-                j++;
-        } 
+            if (list1.get(l1) < list2.get(l2)) {
+                l1++;
+            } else {
+                l2++;
+            }
+        }
         return min;
     }
 }
-
 /**
  * Your WordDistance object will be instantiated and called as such:
  * WordDistance obj = new WordDistance(wordsDict);
