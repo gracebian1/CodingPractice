36f323d737b74f10e2303ad49136c04fc0f6bf87
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2022-07-13 20:54:40 -0400

    add 219

--- 219_contains-duplicate-ii.java
+++ 219_contains-duplicate-ii.java
@@ -1,40 +1,43 @@
 class Solution {
+    /*
+    // Time: O(n), Space: O(n)
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || nums.length == 1) return false;
-        if (k <= 0) return false;       // Two distinct indices
+        if (nums == null || nums.length == 0 || nums.length == 1)
+            return false;
         
-        // Map key--value => Array value---index
+        if (k <= 0)
+            return false;
+        
+        // value: index
         Map<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i]) && (i - map.get(nums[i])) <= k) {
+            if (map.containsKey(nums[i]) && (i - map.get(nums[i]) <= k))
                 return true;
-            }
-            // Attention: map value is array index (not frequency of num)
-            map.put(nums[i], i);    
-        }
-        return false;
-    }
-
-    /*
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || nums.length == 1) return false;
-        if (k <= 0) return false;          // Two distinct indices
-        
-        Set<Integer> set = new HashSet<>();
-        
-        for (int i = 0; i < nums.length; i++) {
             
-            // If the gap is larger than k distance
-            if (i > k) {
-                set.remove(nums[i - k - 1]);
-            }
-            // All elements within k distance is in set
-            if (!set.add(nums[i])) {
-                return true;
-            }
+            map.put(nums[i], i);
         }
         return false;
     }
     */
+    
+    // Time: O(n), Space: O(min(n, k))
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || nums.length == 1)
+            return false;
+        if (k <= 0)
+            return false;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i]))
+                return true;
+            set.add(nums[i]);
+            
+            if (set.size() > k)
+                set.remove(nums[i - k]);
+        }
+        return false;
+    } 
 }
