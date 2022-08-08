8716175700f4b8522b3a98ed0da6b0b208eb81db
Grace Bian <67076065+gracebian1@users.noreply.github.com>
2022-08-07 20:40:20 -0400

    add 227

--- 227_basic-calculator-ii.java
+++ 227_basic-calculator-ii.java
@@ -1,45 +1,79 @@
 class Solution {
+    /*
+    // Using stack: Time: O(n), Space: O(n)
+    
     public int calculate(String s) {
         if (s == null || s.length() == 0)
             return 0;
         
-        // Integer, not Character, on stack
         Stack<Integer> stack = new Stack<>();
         
-        int result = 0, currNum = 0;
-        char currOperation = '+';
+        int currNum = 0, result = 0;
+        char currSign = '+';
         
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             
-            // For number of multiple digits
             if (Character.isDigit(c)) {
                 currNum = currNum * 10 + (c - '0');
             }
             
-            // c is one of 4 operations, OR is last index of string s
             if (c != ' ' && !Character.isDigit(c) || i == s.length() - 1) {
-                if (currOperation == '+')
+                if (currSign == '+') {
                     stack.push(currNum);
-                if (currOperation == '-')
+                }
+                if (currSign == '-') {
                     stack.push(-currNum);
-                
-                // Follow operator precedence
-                if (currOperation == '*')
+                }
+                if (currSign == '*') {
                     stack.push(stack.pop() * currNum);
-                if (currOperation == '/')
+                }
+                if (currSign == '/') {
                     stack.push(stack.pop() / currNum);
+                }
                 
-                // Update the operation sign
-                currOperation = c;
-                // Update the current number 
                 currNum = 0;
+                currSign = c;
             }
         }
-    
-        while (!stack.isEmpty()) 
-            result += stack.pop();
         
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;   
+    }
+    */   
+    // NOT using stack: Time: O(n), Space: O(1)
+    public int calculate(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        
+        int curr = 0, prev = 0, result = 0;
+        char sign = '+';
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (Character.isDigit(c)) {
+                curr = curr * 10 + (c - '0');
+            }
+            
+            // ALSO need to check for the last position of string
+            if (!Character.isDigit(c) && c != ' ' || i == s.length() - 1) {
+                if (sign == '+' || sign == '-') {
+                    result += prev;
+                    prev = (sign == '+') ? curr : -curr;
+                } else if (sign == '*') {
+                    prev = prev * curr; 
+                } else if (sign == '/') {
+                    prev = prev / curr;
+                }
+                
+                curr = 0;
+                sign = c;
+            }
+        }
+        result += prev;
         return result;
     }
 }
